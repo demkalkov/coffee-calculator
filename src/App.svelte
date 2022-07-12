12ea8051df1c60e2403e9723f90ed8e5d432de3f
Dementiy Kalkov <dementiy.kalkov@gmail.com>
2022-07-13 01:00:25 +0300

    chore: basic styling and calculation feat: initialize

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import "./app.css";
+  import logo from './assets/coffee.svg'
+  import Calculator from './lib/Calculator.svelte'
+</script>
+
+<main class="p-4 text-lg">
+  <div class="flex justify-center">
+    <img src={logo} alt="Cofee cup" />
+  </div>
+
+  <Calculator />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
---
+++ src/lib/Calculator.svelte
@@ -0,0 +1,69 @@
+<script>
+  let ratio = 16.67;
+  let weight = 30;
+  let volume = 0;
+  let type = "volume";
+
+  const calculate = (r, w, v, t) => {
+    if (t === "volume") {
+      return Math.round(w * r * 100) / 100;
+    }
+
+    if (t === "weight") {
+      return Math.round(v / r * 100) / 100;
+    }
+
+    return Math.round(v / w * 100) / 100;
+  };
+
+  const onChange = () => {
+    if (type === "volume") {
+      volume = calculate(ratio, weight, volume, type);
+    }
+
+    if (type === "weight") {
+      weight = calculate(ratio, weight, volume, type);
+    }
+
+    if (type === "ratio") {
+      ratio = calculate(ratio, weight, volume, type);
+    }
+  };
+
+  onChange();
+</script>
+
+<div class="grid auto-cols-[min_auto] gap-x-4 gap-y-2">
+  <strong class="col-span-2">Ratio</strong>
+  <div>
+    <input type="radio" value="ratio" bind:group={type}/>
+  </div>
+  <div>
+    <input
+      type="number"
+      class="border rounded-sm border-gray-400"
+      bind:value={ratio} on:change={onChange} />
+  </div>
+
+  <strong class="col-span-2">Weight</strong>
+  <div>
+    <input type="radio" value="weight" bind:group={type}/>
+  </div>
+  <div>
+    <input
+      type="number"
+      class="border rounded-sm border-gray-400"
+      bind:value={weight} on:change={onChange}/>
+  </div>
+
+  <strong class="col-span-2">Volume</strong>
+  <div>
+    <input type="radio" value="volume" bind:group={type} />
+  </div>
+  <div>
+    <input
+      type="number"
+      class="border rounded-sm border-gray-400"
+      bind:value={volume} on:change={onChange}/>
+  </div>
+</div>
