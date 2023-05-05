a53b213ecb6c61d8d1897e4375db1ee55f9be37a
Dementiy Kalkov <dementiy.kalkov@gmail.com>
2023-05-06 01:20:23 +0400

    Deploy

---
+++ _next/static/2qxHHrG7X_0uyiTA5vuvS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/37ec3a5cd9a53adf.css","static/chunks/pages/index-aaf5892417bd1abc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
